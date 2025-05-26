a14397d48e02cdac7a7e0c97b2acd6b1e077c29e
Levent <levent.cracea@gmail.com>
2025-05-26 10:37:07 +0200

    ap left ui fixed, player turn fixed,
    fixing turn counter

--- Assets/Scripts/GamplayUiController.cs
+++ Assets/Scripts/GamplayUiController.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class GameplayUIController : MonoBehaviour
 {
+    public TextMeshProUGUI turnIndicatorText;
+    public TextMeshProUGUI apIndicatorText;
     public void OnDefendButtonPressed()
     {
         Token selected = TokenSelector.Instance?.GetSelectedToken();
@@ -18,5 +20,18 @@ public class GameplayUIController : MonoBehaviour
     public void OnResetGameButtonPressed()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void UpdateTurnIndicator(string playerName, Color playerColor)
+    {
+        if (turnIndicatorText != null)
+        {
+            turnIndicatorText.text = $"Turn: {playerName}";
+            turnIndicatorText.color = playerColor;
+        }
+    }
+    public void UpdateAPIndicator(int currentAP)
+    {
+        if (apIndicatorText != null)
+            apIndicatorText.text = $"AP: {currentAP}";
     }
 }
